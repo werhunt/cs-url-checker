d0a6ab758601d357bb821a3166c1238436bf8cf4
deathnmind <mike.burton@gmail.com>
2021-10-27 11:13:34 -0400

    Update cschecker.py
    Using splunk's excepted CSV reader/writer method

--- cschecker.py
+++ cschecker.py
@@ -1,82 +1,51 @@
 #!/usr/bin/env python3
 
 import re
-import argparse
-from csv import DictWriter
+import csv
 import sys
 
 URL_RE = re.compile(r'https?://[^/]+/(.{4})\b')
-
-
-def build_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-p',
-        '--print',
-        default=False,
-        action='store_true',
-        help='Print output to stdout'
-    )
-    parser.add_argument(
-        'url',
-        help='URL to check if its a CS beacon'
-    )
-    global args
-    args = parser.parse_args()
 
 
 def Checksum8(data):
     return sum(map(ord, data))
 
 
-def cs_return(data):
-    '''
-
-    add dictionary stuff here
-    replace print and return string with dic
-
-    '''
-    out = sys.stdout
-    dic = {'combined_uri':url, 'parsedcheck':data}
-    writer = DictWriter(out, fieldnames=dic.keys())
-
-
-    if args.print:
-        writer.writeheader()
-        writer.writerow(dic)
-        #print(data)
-    else:
-        return data
-
-
 def Checksum8LSB(data):
     dMetasploitConstants = {
-        92: 'URI_CHECKSUM_INITW / CS x86',
-        93: 'CS x64',
-        80: 'URI_CHECKSUM_INITP',
-        88: 'URI_CHECKSUM_INITJ',
-        98: 'URI_CHECKSUM_CONN',
-        95: 'URI_CHECKSUM_INIT_CONN',
+        92: 'Possible Windows/Native Client', #URI_CHECKSUM_INITW / CS x86',
+        93: 'Possible CS x64',
+        80: 'Possible Python Client', #URI_CHECKSUM_INITP',
+        88: 'Possible Java Client', #URI_CHECKSUM_INITJ',
+        98: 'Possible Exisiting Session', #URI_CHECKSUM_CONN',
+        95: 'Possible New Stageless Session', #URI_CHECKSUM_INIT_CONN',
     }
     value = Checksum8(data) % 0x100
     return (value, dMetasploitConstants.get(value, ''))
 
 
-def metatool(url):
-    oSearch = URL_RE.search(url)
-    if oSearch != None:
-        path = oSearch.groups()[0]
-        checksumValue, checksumConstant = Checksum8LSB(path)
-        if checksumConstant != '':
-            return '%s (0x%02x)' % (checksumConstant, checksumValue)
+def metatool():
+    if len(sys.argv) != 2:
+        print("Usage pythin metatool.py [uri]")
+        sys.exit(1)
+    infile = sys.stdin
+    outfile = sys.stdout
+    r = csv.DictReader(infile)
+    w = csv.DictWriter(outfile, fieldnames=r.fieldnames)
+    w.writeheader()
+    for result in r:
+        oSearch = URL_RE.search(result['combined_uri'])
+        if oSearch != None:
+            path = oSearch.groups()[0]
+            checksumValue, checksumConstant = Checksum8LSB(path)
+            if checksumConstant != '':
+                result['parsedcheck'] = f'{checksumConstant}'
+            else:
+                result['parsedcheck'] = 'checksum not found'
         else:
-            return 'checksum not found'
-    else:
-        return 'not evaluated'
+            result['parsedcheck'] = 'not evaluated'
+        w.writerow(result)
 
 
 if __name__ == '__main__':
-    build_args()
-    url = args.url
-    t = metatool(url)
-    cs_return(t)
+    metatool()
